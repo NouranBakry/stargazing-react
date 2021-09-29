63efe81acc38508d67868c33a5947f05873560d6
NouranBakry <nouranaymanbakry@gmail.com>
2021-09-29 21:30:16 +0200

    +cleanup

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -6,7 +6,6 @@ export default function NasaPhoto(){
 
     useEffect(()=>{
         fetchPhoto();
-
         async function fetchPhoto(){
             const apiKey = process.env.REACT_APP_NASA_KEY;
             const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`);
