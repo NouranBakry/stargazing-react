38d63d3b608365e2b047daabaf86bdbed02035c8
NouranBakry <nouranaymanbakry@gmail.com>
2021-10-03 15:29:14 +0200

    fix for api key

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from "react";
 
+const apiKey = process.env.REACT_APP_NASA_PHOTO_KEY;
+
 export default function NasaPhoto(){
 
     const [photoData, setPhotoData] = useState(null);
@@ -7,20 +9,21 @@ export default function NasaPhoto(){
     useEffect(()=>{
         fetchPhoto();
         async function fetchPhoto(){
-            const apiKey = process.env.REACT_APP_NASA_KEY;
-            const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`);
+            const res = await fetch(
+                `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`);
             const data = await res.json();
             setPhotoData(data);
+            console.log(data);
         }
     }, []);
-    if(!photoData) return <div></div>;
+    if(!photoData) return <div/>;
     return(
         <div className="nasa-photo">
             {photoData.media_type === "image" ? (<img 
             src={photoData.url}
             alt={photoData.title}
             className="photo"/>) : 
-            (<iframe title="space-video" src="{photoData.url}" frameBorder={0} gesture="media" allow="encrypted-media" allowFullScreen classname="photo">
+            (<iframe allow="autoplay" title="space-video" src="{photoData.url}" frameBorder={0} allowFullScreen className="photo">
             </iframe>)}
             <div>
                 <h1>{photoData.title}</h1>
